65c3dc66c4e5f3e759871e6e105a17fe083f8749
ordericvit <orderic.vit@gmail.com>
2020-06-14 17:48:15 +0300

    Delete docker_machine_start_command.sh

--- docker_machine_start_command.sh
+++
@@ -1,3 +0,0 @@
-!#/bin/bash
-
-docker-machine -D create --driver amazonec2 --amazonec2-access-key $AWS_ACCESS_KEY_ID --amazonec2-secret-key  $AWS_SECRET_ACCESS_KEY --amazonec2-vpc-id vpc-936e3fe9 --amazonec2-instance-type t2.micro --amazonec2-subnet-id subnet-0f5f5e53 docker
